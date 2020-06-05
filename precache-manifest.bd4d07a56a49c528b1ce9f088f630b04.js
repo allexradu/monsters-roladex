5d856ab0f1ca19b8f82691b8d6c7a519fdb3fc33
allexradu <eusceptic@gmail.com>
2020-06-05 20:54:22 +0300

    Updates

---
+++ precache-manifest.bd4d07a56a49c528b1ce9f088f630b04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb0ec5d9cff2ce00a132c0220a912fc",
+    "url": "/monsters-roladex/index.html"
+  },
+  {
+    "revision": "29ad7cfeb3aefb95abc9",
+    "url": "/monsters-roladex/static/css/main.53f2be47.chunk.css"
+  },
+  {
+    "revision": "9d36cb1448d0cd9bbf34",
+    "url": "/monsters-roladex/static/js/2.9d52131b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roladex/static/js/2.9d52131b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ad7cfeb3aefb95abc9",
+    "url": "/monsters-roladex/static/js/main.6018b38a.chunk.js"
+  },
+  {
+    "revision": "ce80dadaecda6863a58a",
+    "url": "/monsters-roladex/static/js/runtime-main.43d7ebee.js"
+  }
+]);
